341b9fee54981acf9ab2aa20662ecaf6c4f9e306
🚀 Yasir N <malaikaanisar2521@gmail.com>
2026-03-06 03:29:37 +0500

    Update _document.tsx

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,6 +5,7 @@ export default function Document() {
     <Html className="h-full antialiased" lang="en">
       <Head>
         <meta charSet="utf-8" />
+        <meta name="google-site-verification" content="zKx4U54EbHN2vUxCUIXfOi2FC0KdJa-vVOdmlCHqItk" />
         <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png" />
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png" />
         <link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png" />
