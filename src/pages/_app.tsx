54714f7557f8e027bc33af31af7e19ce08eb8df1
🚀 Yasir N <malaikaanisar2521@gmail.com>
2026-03-06 05:32:20 +0500

    fix: update import paths for Analytics and SpeedInsights components

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,5 @@
-import { Analytics } from '@vercel/analytics/next';
-import { SpeedInsights } from '@vercel/speed-insights/next';
+import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from '@vercel/speed-insights/react';
 import 'focus-visible';
 import { DefaultSeo } from 'next-seo';
 import { ThemeProvider } from 'next-themes';
